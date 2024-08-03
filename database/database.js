a37da2ed6115e7a7d564c56f6d1031deee45924c
Prabesh-Sharma <prabeshsharma26@gmail.com>
2024-08-03 19:58:39 +0545

    added .env file

--- database/database.js
+++ database/database.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 exports.connectDatabase = async () => {
     try {
-        await mongoose.connect("mongodb+srv://prabeshsharma26:hello@cluster0.kmsqw3r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+        await mongoose.connect(process.env.MONGO_URL, {
         });
         console.log('Database connected successfully');
     } catch (error) {
